f8b8079749bd586d41b3498c705b55fdf2e7a680
w4a2y4 <b04902079@ntu.edu.tw>
2017-05-21 12:19:41 +0800

    Create google form & get key

---
+++ javascript/submitForm.js
@@ -0,0 +1,13 @@
+/*
+
+form : https://goo.gl/forms/xUf8qNovcHDSDjOi1
+edit form : https://docs.google.com/forms/d/1UWUE5VMe-dV6smiHM5Rjz6jvlW4sXhMiq71i_csk3jU/edit?usp=sharing
+form response :
+	https://docs.google.com/forms/d/e/1FAIpQLSdQdenlC7wFmfJULSVtBtqeabLNstC7JaNBRmcStv3SYG79yg/formResponse"
+Dept : entry.386904180
+Sex : entry.2027773850
+Age : entry.1133005454
+
+db : https://docs.google.com/spreadsheets/d/1RimxKNCGFYG3gjyBwaHMuE-rNwBnS2rYxjKKAyWjulo/pubhtml?alt=json
+
+*/
