664eb6b31f352bbfeaf04a04962c35cc19ec6ad8
w4a2y4 <b04902079@ntu.edu.tw>
2017-05-22 00:33:02 +0800

    Send timeForm to google sheet (untested)

--- javascript/submitForm.js
+++ javascript/submitForm.js
@@ -12,13 +12,41 @@ db : https://docs.google.com/spreadsheets/d/1RimxKNCGFYG3gjyBwaHMuE-rNwBnS2rYxjK
 
 */
 
+const DeptID = '386904180';
+const SexID = '2027773850';
+const AgeID = '1133005454';
+
+const Q1ID = '';
+const Q2ID = '';
+const Q3ID = '';
+const Q4ID = '';
+const Q5ID = '';
+const Q6ID = '';
+const Q7ID = '';
+const Q8ID = '';
+
+const 
+
 // get basicInfo from localStorage
 function sendData() {
+
+	var str = "https://docs.google.com/forms/d/e/1FAIpQLSdQdenlC7wFmfJULSVtBtqeabLNstC7JaNBRmcStv3SYG79yg/formResponse?";
+	
 	var basicInfo = JSON.parse(window.localStorage.getItem('basicInfo'));
-	var str = "https://docs.google.com/forms/d/e/1FAIpQLSdQdenlC7wFmfJULSVtBtqeabLNstC7JaNBRmcStv3SYG79yg/formResponse";
-	str += '?entry.386904180=' + basicInfo.DEPT;
-	str += '&entry.2027773850=' + basicInfo.SEX;
-	str += '&entry.1133005454=' + basicInfo.AGE;
-	str += '&submit=Submit';
+	str += 'entry.' + DeptID + '=' + basicInfo.DEPT + '&';
+	str += 'entry.' + SexID + '=' + basicInfo.SEX + '&';
+	str += 'entry.' + AgeID + '=' + basicInfo.AGE + '&';
+
+	var timeForm = JSON.parse(window.localStorage.getItem('timeForm'));
+	str += 'entry.' + Q1ID + '=' + basicInfo.Q1 + '&';
+	str += 'entry.' + Q2ID + '=' + basicInfo.Q2 + '&';
+	str += 'entry.' + Q3ID + '=' + basicInfo.Q3 + '&';
+	str += 'entry.' + Q4ID + '=' + basicInfo.Q4 + '&';
+	str += 'entry.' + Q5ID + '=' + basicInfo.Q5 + '&';
+	str += 'entry.' + Q6ID + '=' + basicInfo.Q6 + '&';
+	str += 'entry.' + Q7ID + '=' + basicInfo.Q7 + '&';
+	str += 'entry.' + Q8ID + '=' + basicInfo.Q8 + '&';
+
+	str += 'submit=Submit';
 	$.getScript(str);
 }
