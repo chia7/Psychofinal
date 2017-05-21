f1fc6659d69f7cceb7eaea139f68a25f689adee7
w4a2y4 <b04902079@ntu.edu.tw>
2017-05-21 17:43:13 +0800

    Call sendData() and the form response would be write to google-sheet

--- javascript/submitForm.js
+++ javascript/submitForm.js
@@ -11,3 +11,14 @@ Age : entry.1133005454
 db : https://docs.google.com/spreadsheets/d/1RimxKNCGFYG3gjyBwaHMuE-rNwBnS2rYxjKKAyWjulo/pubhtml?alt=json
 
 */
+
+// get basicInfo from localStorage
+function sendData() {
+	var basicInfo = JSON.parse(window.localStorage.getItem('basicInfo'));
+	var str = "https://docs.google.com/forms/d/e/1FAIpQLSdQdenlC7wFmfJULSVtBtqeabLNstC7JaNBRmcStv3SYG79yg/formResponse";
+	str += '?entry.386904180=' + basicInfo.DEPT;
+	str += '&entry.2027773850=' + basicInfo.SEX;
+	str += '&entry.1133005454=' + basicInfo.AGE;
+	str += '&submit=Submit';
+	$.getScript(str);
+}
